30d3993a0e4b1444d80286d933557a537b9cbcae
BergerAPI <bergerbtw@gmail.com>
2021-09-08 21:42:08 +0200

    basic generator

---
+++ source/gen/generator.ts
@@ -0,0 +1,30 @@
+import { AstTree } from "../parser";
+
+/**
+ * This converts json ast to thingy c++ code
+ */
+export class Generator {
+
+    /**
+     * We need information about this little pog thingy
+     */
+    ast: AstTree;
+
+    constructor(ast: AstTree) {
+        this.ast = ast;
+    }
+
+    /**
+     * We want to full c++ code like cool thingys
+     */
+    toString() {
+
+    }
+
+    /**
+     * Here happens the magic. pog
+     */
+    work() {
+
+    }
+}
--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,7 @@
 import { AstTree, Parser } from "./parser"
 import { Lexer } from "./lexer"
 import * as fs from 'fs';
+import { Generator } from "./gen/generator";
 
 /**
  * Reading the input file
@@ -14,5 +15,6 @@ let content = readFile("./tests/basic.lang")
 // Here begins the fun
 const lexer = new Lexer(content)
 const tree = new AstTree("Program", new Parser(lexer))
+const generator = new Generator(tree)
 
-console.log(tree.toString())
+console.log(generator.toString())
