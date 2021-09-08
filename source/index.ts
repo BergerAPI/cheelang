cc93d96d9d7e09604a1402e9f5cdf156539c8935
BergerAPI <bergerbtw@gmail.com>
2021-09-08 21:38:08 +0200

    generator

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,7 @@
 import { AstTree, Parser } from "./parser"
 import { Lexer } from "./lexer"
 import * as fs from 'fs';
+import { Generator } from "./gen/generator";
 
 /**
  * Reading the input file
@@ -12,7 +13,8 @@ function readFile(path: string) {
 let content = readFile("./tests/basic.lang")
 
 // Here begins the fun
-const lexer = new Lexer(content)
-const tree = new AstTree("Program", new Parser(lexer))
+const lexer = new Lexer(content);
+const tree = new AstTree("Program", new Parser(lexer));
+const generator = new Generator(tree);
 
-console.log(tree.toString())
+console.log(generator.toString())
